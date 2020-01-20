a3e0487716c1cd185db45c0dfd691233dacbb738
minhtran365411 <g00365411@gmit.ie>
2020-01-20 17:28:33 +0000

    added for loop

--- src/ie/gmit/packagename/HelloWorld.java
+++ src/ie/gmit/packagename/HelloWorld.java
@@ -3,8 +3,11 @@ package ie.gmit.packagename;
 class HelloWorld {
 
     public static void main(String[] args) {
-        // Print Hello World! to the console.
-        System.out.println("Hello World!");
+        // Print Hello World! to the console
+        for (int i = 1; i <= 10; i++) {
+        System.out.println(i+". Hello World!");
+        }
+
     }
 
 }
