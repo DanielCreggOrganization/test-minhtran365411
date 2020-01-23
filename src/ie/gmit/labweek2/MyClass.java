07d40866167e56c01c1fb8e5c793fb45e0bd7bb8
minhtran365411 <g00365411@gmit.ie>
2020-01-23 14:12:15 +0000

    new package

---
+++ src/ie/gmit/labweek2/MyClass.java
@@ -0,0 +1,15 @@
+package ie.gmit.labweek2;
+
+class MyClass {
+/* 
+    public myMethod() {
+        System.out.println("I just got executed!");
+        m = new myMethod();
+        m.myMethod();
+    }*/
+
+    public static void main(String[] args) {
+        
+        System.out.println("I just got executed!");
+    }
+}
