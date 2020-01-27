43d7cb720c6d0d957ca6125c034fc5382ed3b6aa
minhtran365411 <g00365411@gmit.ie>
2020-01-27 16:49:27 +0000

    change

---
+++ src/ie/gmit/labweek3/variablesType.java
@@ -0,0 +1,24 @@
+package ie.gmit.labweek3;
+
+public class variablesType {
+    public static void main(String[] args) {
+        boolean alive = true;
+        byte age = 20;
+        char iniName = "M";
+        short height = 156;
+        int phoneNo = 0832212000;
+        long longNo = 134546843131300549;
+        float floatNo = 1.11f;
+        double doubleNo = -265691.15;
+
+        System.out.println("All variables' names & values are: ");
+        System.out.println("alive = " + alive);
+        System.out.println("iniName = " + iniName);
+        System.out.println("age = " + age);
+        System.out.println("height = " + height);
+        System.out.println("phoneNo = " + phoneNo);
+        System.out.println("longNo = " + longNo);
+        System.out.println("floatNo = " + floatNo);
+        System.out.println("doubleNo = " + doubleNo);
+    }
+}
