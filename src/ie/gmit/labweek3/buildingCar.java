175221f7985c0a399cea85003f173a4a38bcd50c
minhtran365411 <g00365411@gmit.ie>
2020-01-27 17:36:01 +0000

    car

---
+++ src/ie/gmit/labweek3/buildingCar.java
@@ -0,0 +1,35 @@
+package ie.gmit.labweek3;
+
+public class buildingCar {
+    //Creating instance variables
+    int topSpeed;
+    int totalSeats;
+    int fuelCapicity;
+    String manufacturer;
+    String owner;
+    float engineSize;
+    char carClass;
+    float value;
+
+    //Class Methods
+
+    void refuel() {
+        
+    }
+
+    void park() {
+
+    }
+
+    void drive() {
+
+    }
+
+    void crash() {
+        value = 0;
+    }
+    
+    void show() {
+        System.out.println("Manufacturer: "+manufacturer);
+    }
+}
