e196c7e4c4a4699d23c8ac24ff3427829182bdc9
minhtran365411 <g00365411@gmit.ie>
2020-01-27 17:44:25 +0000

    change

---
+++ src/ie/gmit/labweek3/Main.java
@@ -0,0 +1,17 @@
+package ie.gmit.labweek3;
+
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car(); 
+        Car car2 = new Car(); 
+
+        car1.value = 10000;
+        car2.value = 20000;
+
+        System.out.println(car1.value);
+    }
+    
+}
