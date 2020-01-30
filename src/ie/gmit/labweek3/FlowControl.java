aa78d9da72033793e989b15d6c383e9e1c54b195
minhtran365411 <g00365411@gmit.ie>
2020-01-30 13:51:26 +0000

    x

--- src/ie/gmit/labweek3/FlowControl.java
+++ src/ie/gmit/labweek3/FlowControl.java
@@ -3,11 +3,59 @@ import java.util.Scanner;
 
 public class FlowControl {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the number of your birth month: ");
-        Byte month = input.nextByte();
+        int month = sc.nextInt();
     }
 
-    
+    void birthMonthFinder(int month) {
+        switch (month) {
+            case 1:
+                System.out.println("You were born in January.");
+            break;
+            case 2:
+                System.out.println("You were born in February.");
+            break;
+            case 3:
+                System.out.println("You were born in March.");
+            break;
+            case 4:
+                System.out.println("You were born in April.");
+            break;
+            case 5:
+                System.out.println("You were born in May.");
+            break;
+            case 6:
+                System.out.println("You were born in June.");
+            break;
+            case 7:
+                System.out.println("You were born in July.");
+            break;
+            case 8:
+                System.out.println("You were born in August.");
+            break;
+            case 9:
+                System.out.println("You were born in September.");
+            break;
+            case 10:
+                System.out.println("You were born in October.");
+            break;
+            case 11:
+                System.out.println("You were born in November.");
+            break;
+            case 12:
+                System.out.println("You were born in December.");
+            break;
+            default:
+                System.out.println("You were born in January.");
+            break;
+            case 1:
+                System.out.println("");
+            break;
+        }
+    }
+
+
+
 }
