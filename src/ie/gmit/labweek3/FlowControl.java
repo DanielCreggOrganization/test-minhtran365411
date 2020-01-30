6bb804d96f16130ea120cf1876ee40a3e89245a1
minhtran365411 <g00365411@gmit.ie>
2020-01-30 13:43:51 +0000

    new

---
+++ src/ie/gmit/labweek3/FlowControl.java
@@ -0,0 +1,13 @@
+package ie.gmit.labweek3;
+import java.util.Scanner;
+
+public class FlowControl {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of your birth month: ");
+        Byte month = input.nextByte();
+    }
+
+    
+}
