242cb0b9d5cefe5a3fe2e11333612ca2af691c4b
minhtran365411 <g00365411@gmit.ie>
2020-01-30 14:28:03 +0000

    j

--- src/ie/gmit/labweek3/ForLoopIte.java
+++ src/ie/gmit/labweek3/ForLoopIte.java
@@ -6,19 +6,30 @@ public class ForLoopIte {
         Scanner input = new Scanner(System.in);
         ForLoopIte a = new ForLoopIte();
 
-        System.out.print("We are going to count from 1 to N.\n PLease enter N: ");
+        System.out.print("We are going to count from 1 to N.\nPLease enter N: ");
         int n = input.nextInt();
 
-        a.counter(n);
+        a.counterFor(n);
         input.close();
 
     }
 
-    void counter(int n) {
+    void counterFor(int n) {
     String output = "";
         for (int i = 1; i <= n; i++) {
             output += i +"\n";
         }
-    System.out.println(output);
+    System.out.println("Print by for loop: \n"+output);
     } 
+
+    void counterWhile(int n) {
+        String output = "";
+        int i = 1;
+
+        while (i <= n) {
+            output += i +"\n";
+            i++;
+        }
+        System.out.println("Print by while loop: \n"+output);
+    }
 }
