fe474b2adcf312fd3ab3ab36c270801f1a54d575
minhtran365411 <g00365411@gmit.ie>
2020-01-30 14:09:54 +0000

    d

--- src/ie/gmit/labweek3/FlowControl.java
+++ src/ie/gmit/labweek3/FlowControl.java
@@ -11,11 +11,13 @@ public class FlowControl {
         System.out.print("Enter the number of your birth month: ");
         month = sc.nextInt();
 
-        a.birthMonthFinder(month);
+        a.birthMonthFinderSwitch(month);
+        a.birthMonthFinderIfElse(month);
         sc.close();
     }
 
-    public void birthMonthFinder(int month) {
+    //write with Switch
+    public void birthMonthFinderSwitch(int month) {
         switch (month) {
             case 1:
                 System.out.println("You were born in January.");
@@ -59,6 +61,38 @@ public class FlowControl {
         }
     }
 
+//write with if-else
+    public void birthMonthFinderIfElse(int month) {
+        if (month >=1 && month <= 12) {
+            if (month == 1) {
+                System.out.println("You were born in January.");
+            } else if (month == 2) {
+                System.out.println("You were born in February.");
+            } else if (month == 3) {
+                System.out.println("You were born in March.");
+            } else if (month == 4) {
+                System.out.println("You were born in April.");
+            } else if (month == 5) {
+                System.out.println("You were born in May.");
+            } else if (month == 6) {
+                System.out.println("You were born in June.");
+            } else if (month == 7) {
+                System.out.println("You were born in July.");
+            } else if (month == 8) {
+                System.out.println("You were born in August.");
+            } else if (month == 9) {
+                System.out.println("You were born in September.");
+            } else if (month == 10) {
+                System.out.println("You were born in October.");
+            } else if (month == 11) {
+                System.out.println("You were born in November.");
+            } else {
+                System.out.println("You were born in December.");
+            }
+        } else {
+            System.out.println("Invalid number - please enter number from 1 to 12.");
+        }
+    }
 
 
 }
