281e76dfe55bfc4f6b3730969ae68d9bcf36a502
minhtran365411 <g00365411@gmit.ie>
2020-01-30 14:19:24 +0000

    forloop

---
+++ src/ie/gmit/labweek3/ForLoopIte.java
@@ -0,0 +1,24 @@
+package ie.gmit.labweek3;
+import java.util.Scanner;
+
+public class ForLoopIte {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ForLoopIte a = new ForLoopIte();
+
+        System.out.print("We are going to count from 1 to N./n PLease enter N: ");
+        int n = input.nextInt();
+
+        a.counter(n);
+        input.close();
+
+    }
+
+    void counter(int n) {
+    String output = "";
+        for (int i = 1; i <= n; i++) {
+            output += i +"/n";
+        }
+    System.out.println(output);
+    } 
+}
