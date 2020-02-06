f4453985822a01bf3ccec8716299bc3fd86a84a4
minhtran365411 <g00365411@gmit.ie>
2020-02-06 13:33:53 +0000

    g

---
+++ src/ie/gmit/labweek4/Chess.java
@@ -0,0 +1,13 @@
+package ie.gmit.labweek4;
+
+class Chess {
+    public static void main(String[] args) {
+    char[][] board = { {'a','b','c'},
+                       {'a','b','c'},
+                       {'a','b','c'} };
+    
+    char x = board[2][2];
+
+    System.out.println(x);
+}
+}
