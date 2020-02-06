466e2b8e843306aa2ff016e5b0b09ec9329d2081
minhtran365411 <g00365411@gmit.ie>
2020-02-06 14:15:09 +0000

    d

--- src/ie/gmit/labweek4/Chess.java
+++ src/ie/gmit/labweek4/Chess.java
@@ -2,21 +2,36 @@ package ie.gmit.labweek4;
 
 class Chess {
     public static void main(String[] args) {
-    char[][] board = { {'a','b','c'},
-                       {'a','b','c'},
-                       {'a','b','c'} };
-    
-    char x = board[2][2];
+    int[][] board = new int[8][8]; 
 
-    System.out.println(x);
-
-    board[1][2] = 'd';
-    System.out.println(board[1][2]);
+    // 1 is black tile, 2 is white tile
+    for (int i = 0; i < board.length; i++ ) {
+        if ((i%2) == 0 ) {
+            for(int j = 0; j < board[i].length; j++) {
+                if ((j%2) == 0 ) {
+                    board[i][j] = 1;
+                }  else {
+                    board[i][j] = 2;
+                }
+            }
+        } else {
+            for(int j = 0; j < board[i].length; j++) {
+                if ((j%2) == 0 ) {
+                    board[i][j] = 2;
+                }  else {
+                    board[i][j] = 1;
+                }
+            }
+        }
+       
+    }
 
     for (int i = 0; i < board.length; i++ ) {
         for(int j = 0; j < board[i].length; j++) {
-            System.out.println(board[i][j]);
+            System.out.print(board[i][j]+" ");
         }
+        System.out.println();
     }
+    
 }
 }
