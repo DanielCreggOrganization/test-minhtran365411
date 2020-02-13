fa59a445ce9813c4fa9df0be767c653d66151dd4
minhtran365411 <g00365411@gmit.ie>
2020-02-13 13:41:55 +0000

    d

---
+++ src/ie/gmit/labweek5/Main.java
@@ -0,0 +1,11 @@
+package ie.gmit.labweek5;
+
+public class Main {
+    public static void main(String[] args) {
+        Human humanObject1 = new Human();
+        humanObject1.name = "Minh";
+        humanObject1.age = 20;
+
+        System.out.println(humanObject1.name + " is " + humanObject1.age);
+    }
+}
