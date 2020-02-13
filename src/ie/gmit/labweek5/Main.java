1886f58b5a62fa00a8e4570dbbc1ea9fcf87db78
minhtran365411 <g00365411@gmit.ie>
2020-02-13 14:47:05 +0000

    f

--- src/ie/gmit/labweek5/Main.java
+++ src/ie/gmit/labweek5/Main.java
@@ -2,41 +2,49 @@ package ie.gmit.labweek5;
 
 public class Main {
     public static void main(String[] args) {
-        
-        Human humanObject1 = new Human();
-        Human humanObject2 = new Human();
-        Human humanObject3 = new Human();
-        Human humanObject4 = new Human();
+        Puppy puppyObject1 = new Puppy();
+        Puppy puppyObject2 = new Puppy("Bingo");
+        Puppy puppyObject3 = new Puppy("Rex", 10, "Corgi");
 
-        humanObject1.name = "Minh";
-        humanObject1.age = 20;
-        humanObject1.nationality = "Vietnamese";
-        humanObject1.weight = 54.6f;
 
-        System.out.println(humanObject1.name + " is " + humanObject1.age + " and is a " + humanObject1.nationality);
-        humanObject1.sayHello();
-        System.out.println( humanObject1.eating(0.5f) ); 
-        humanObject1.ageGroup();
+        System.out.println(puppyObject1.name + " is " + puppyObject1.age + " and is a " + puppyObject1.breed);
+        System.out.println(puppyObject2.name + " is " + puppyObject2.age + " and is a " + puppyObject2.breed);
+        System.out.println(puppyObject3.name + " is " + puppyObject3.age + " and is a " + puppyObject3.breed);
 
-        humanObject2.name = "David";
-        humanObject2.age = 28;
-        humanObject2.nationality = "Irish";
+        // Human humanObject1 = new Human();
+        // Human humanObject2 = new Human();
+        // Human humanObject3 = new Human();
+        // Human humanObject4 = new Human();
 
-        System.out.println(humanObject2.name + " is " + humanObject2.age + " and is an " + humanObject2.nationality);
-        System.out.println( humanObject2.yearsGoneBy() );
-        humanObject2.ageGroup();
+        // humanObject1.name = "Minh";
+        // humanObject1.age = 20;
+        // humanObject1.nationality = "Vietnamese";
+        // humanObject1.weight = 54.6f;
 
-        humanObject3.name = "Louise";
-        humanObject3.age = 21;
-        humanObject3.nationality = "Frech";
+        // System.out.println(humanObject1.name + " is " + humanObject1.age + " and is a " + humanObject1.nationality);
+        // humanObject1.sayHello();
+        // System.out.println( humanObject1.eating(0.5f) ); 
+        // humanObject1.ageGroup();
 
-        System.out.println(humanObject3.name + " is " + humanObject3.age + " and is a " + humanObject3.nationality);
-        humanObject3.sayHello();
-        humanObject3.ageGroup();
+        // humanObject2.name = "David";
+        // humanObject2.age = 28;
+        // humanObject2.nationality = "Irish";
 
-        humanObject4.name = "John";
-        humanObject4.age = 80;
-        System.out.println(humanObject4.name + " is " + humanObject4.age + " and is a " + humanObject4.nationality);
-        humanObject4.ageGroup();
+        // System.out.println(humanObject2.name + " is " + humanObject2.age + " and is an " + humanObject2.nationality);
+        // System.out.println( humanObject2.yearsGoneBy() );
+        // humanObject2.ageGroup();
+
+        // humanObject3.name = "Louise";
+        // humanObject3.age = 21;
+        // humanObject3.nationality = "Frech";
+
+        // System.out.println(humanObject3.name + " is " + humanObject3.age + " and is a " + humanObject3.nationality);
+        // humanObject3.sayHello();
+        // humanObject3.ageGroup();
+
+        // humanObject4.name = "John";
+        // humanObject4.age = 80;
+        // System.out.println(humanObject4.name + " is " + humanObject4.age + " and is a " + humanObject4.nationality);
+        // humanObject4.ageGroup();
     }
 }
--- src/ie/gmit/labweek5/Puppy.java
+++ src/ie/gmit/labweek5/Puppy.java
@@ -1,7 +1,23 @@
 package ie.gmit.labweek5;
 
 public class Puppy {
-    public Puppy() {
-        
+    String name;
+    int age;
+    String breed;
+    String color;
+
+    //Default Constructor
+    Puppy() {
+
+    }
+
+    Puppy(String newName) {
+        name = newName;
+    }
+
+    Puppy(String newName, int newAge, String newBreed) {
+        name = newName;
+        age = newAge;
+        breed = newBreed;
     }
 }
